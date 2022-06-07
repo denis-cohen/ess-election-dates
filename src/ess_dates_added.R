9451e2523d34daf0b56d993f8299bae825e6b762
Ryohei Mogi <59123976+RyoMogi@users.noreply.github.com>
2022-06-07 20:59:40 +0100

    Added AT 4, 5

--- src/ess_dates_added.R
+++ src/ess_dates_added.R
@@ -10,13 +10,13 @@ ess_election_dates$recent_election[ess_election_dates$cntry == "EE" & ess_electi
 ess_election_dates$recent_election[ess_election_dates$cntry == "RS" & ess_election_dates$essround == 9] <- "2016-04-24"
 
 dates_added <- data.frame(
-  cntry = c("DK", "ES", "HR", "IS", "LT", "LV", "PT", "SE", "SK"),
-  essround = c(9, 9, 9, 9, 9, 9, 9, 9, 9),
+  cntry = c("AT", "AT", "DK", "ES", "HR", "IS", "LT", "LV", "PT", "SE", "SK"),
+  essround = c(4, 5, 9, 9, 9, 9, 9, 9, 9, 9, 9),
   # manual imputation
-  recent_election = as.Date(c("2015-06-18", "2019-11-10", "2016-09-11", "2017-10-28", "2016-10-09", "2018-10-06", "2015-10-04", "2018-09-09", "2016-03-05")),
-  split_wave = rep(NA, 9),
-  recent_election_split1 = as.Date(rep(NA, 9)),
-  max_days_since_election_split1 = rep(NA, 9)
+  recent_election = as.Date(c("2008-09-28", "2008-09-28", "2015-06-18", "2019-11-10", "2016-09-11", "2017-10-28", "2016-10-09", "2018-10-06", "2015-10-04", "2018-09-09", "2016-03-05")),
+  split_wave = rep(NA, 11),
+  recent_election_split1 = as.Date(rep(NA, 11)),
+  max_days_since_election_split1 = rep(NA, 11)
 )
 
 field_date <- ESS9 %>% 
@@ -26,10 +26,11 @@ field_date <- ESS9 %>%
   summarise(field_start = min(field_start, na.rm = T),
             field_end = max(field_end, na.rm = T)) %>% 
   mutate(field_start = as.Date(field_start),
-         field_end = as.Date(field_end))
+         field_end = as.Date(field_end),
+         essround = 9)
     
 dates_added <- dates_added %>% 
-  left_join(field_date, by = "cntry") %>% 
+  left_join(field_date, by = c("cntry", "essround")) %>% 
   mutate(max_days_since_election = round(difftime(field_end, recent_election, units = "days")))
 
 ess_election_dates_added <- ess_election_dates %>% 
